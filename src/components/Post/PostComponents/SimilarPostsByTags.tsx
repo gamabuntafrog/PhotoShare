943d4e4c722691b46fa9763f45c246fdbefdfdb1
Yaroslaw Kyrylenko <kirik.slaw@gmail.com>
2023-05-30 16:30:58 +0300

    added title to similar posts

--- src/components/Post/PostComponents/SimilarPostsByTags.tsx
+++ src/components/Post/PostComponents/SimilarPostsByTags.tsx
@@ -2,8 +2,9 @@ import extendedPostsApi from '../../../redux/api/extendedPostsApi'
 import usePostsActions from '../../../hooks/usePostsActions'
 import useSx from '../../../hooks/useSx'
 import postStyles from '../postStyles'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import MasonryPostsDrawer from '../../MasonryPostsDrawer'
+import useShortTranslation from '../../../hooks/useShortTranslation'
 
 export default function SimilarPostsByTags({ tags, postId }: { tags: string[]; postId: string }) {
   const {
@@ -15,10 +16,15 @@ export default function SimilarPostsByTags({ tags, postId }: { tags: string[]; p
   const [posts, postActions] = usePostsActions({ initPosts: data })
   const { similarPosts: styles } = useSx(postStyles)
 
+  const t = useShortTranslation({ componentNameKey: 'Post.Similar' })
+
   if (isLoading || data.length === 0 || isError) return null
 
   return (
     <Box sx={{ px: 1 }}>
+      <Typography sx={{ margin: 2, fontWeight: 'bold' }} variant="h2">
+        {t('title')}
+      </Typography>
       <MasonryPostsDrawer isNextPostsLoading={false} posts={posts} postsActions={postActions} />
     </Box>
   )
