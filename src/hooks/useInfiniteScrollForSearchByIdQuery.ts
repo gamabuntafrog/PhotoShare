6fdb0075d5e776d0414b6dbb3c319378205351de
Yaroslaw Kyrylenko <kirik.slaw@gmail.com>
2023-05-31 09:39:10 +0300

    fixed bug when navigate from one user profile to next

--- src/hooks/useInfiniteScrollForSearchByIdQuery.ts
+++ src/hooks/useInfiniteScrollForSearchByIdQuery.ts
@@ -27,6 +27,7 @@ export default function useInfiniteScrollForSearchByIdQuery({
   useLayoutEffect(() => {
     setCombinedValue([])
     setIsEnd(false)
+    trigger({ arrayOfId: [], id })
   }, [id])
 
   useLayoutEffect(() => {
